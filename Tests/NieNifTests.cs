3b966718cf01bb151d0b130a1d3ad96dd9b4f557
Immanuel Hayden <immanuel.hayden@gmail.com>
2015-03-10 11:19:21 +0100

    implementation

---
+++ Tests/NieNifTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using idgen.net;
+
+namespace Tests
+{
+    [TestClass]
+    public class NieNifTests
+    {
+        [TestMethod]
+        public void ValidatesItsOwnDnis()
+        {
+            var id = IdGen.GenerateDni();
+            Assert.AreEqual(IdGen.ValidateDni(id), ValidationResult.Valid);
+        }
+        [TestMethod]
+        public void ValidatesItsOwnNif()
+        {
+            var id = IdGen.GenerateNif();
+            Assert.AreEqual(IdGen.ValidateNif(id), ValidationResult.Valid);
+        }
+        [TestMethod]
+        public void ValidatesItsOwnNie()
+        {
+            var id = IdGen.GenerateNie();
+            Assert.AreEqual(IdGen.ValidateNie(id), ValidationResult.Valid);
+        }
+        [TestMethod]
+        public void DniNifAreSame()
+        {
+            var id = IdGen.GenerateNif();
+            Assert.AreEqual(IdGen.ValidateDni(id), ValidationResult.Valid);
+
+            id = IdGen.GenerateDni();
+            Assert.AreEqual(IdGen.ValidateNif(id), ValidationResult.Valid);
+        }
+        [TestMethod]
+        public void NieNifAreNotSame()
+        {
+            var id = IdGen.GenerateNie();
+            Assert.AreEqual(IdGen.ValidateNif(id), ValidationResult.PatternMismatch);
+
+            id = IdGen.GenerateNif();
+            Assert.AreEqual(IdGen.ValidateNie(id), ValidationResult.PatternMismatch);
+        }
+
+        [TestMethod]
+        public void ValidatesRealNif()
+        {
+            const string id = "05291916F";
+            Assert.AreEqual(IdGen.ValidateNif(id), ValidationResult.Valid);
+        }
+
+        [TestMethod]
+        public void NotValidatesBadChecksumNif()
+        {
+            const string id = "05291916A";
+            Assert.AreEqual(IdGen.ValidateNif(id), ValidationResult.ChecksumMismatch);
+        }
+
+        [TestMethod]
+        public void NotValidatesBadLengthNif()
+        {
+            const string id = "005291916F";
+            Assert.AreEqual(IdGen.ValidateNif(id), ValidationResult.PatternMismatch);
+        }
+
+        [TestMethod]
+        public void NotValidatesBadLengthNif2()
+        {
+            const string id = "5291916F";
+            Assert.AreEqual(IdGen.ValidateNif(id), ValidationResult.PatternMismatch);
+        }
+
+        [TestMethod]
+        public void ValidatesRealNie()
+        {
+            const string id = "X1233453D";
+            Assert.AreEqual(IdGen.ValidateNie(id), ValidationResult.Valid);
+        }
+
+        [TestMethod]
+        public void NotValidatesBadLengthNie()
+        {
+            const string id = "X12233453D";
+            Assert.AreEqual(IdGen.ValidateNie(id), ValidationResult.PatternMismatch);
+        }
+
+        [TestMethod]
+        public void NotValidatesBadLengthNie2()
+        {
+            const string id = "X123453D";
+            Assert.AreEqual(IdGen.ValidateNie(id), ValidationResult.PatternMismatch);
+        }
+    }
+}
---
+++ idgen.net/IdGen.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace idgen.net
+{
+    public static class IdGen
+    {
+        private const string NieStartLetters = "XYZ";
+
+        #region Generators
+        public static string GenerateDni()
+        {
+            return GenerateNif();
+        }
+
+        public static string GenerateNif()
+        {
+            var digits = GetRandomDigits(8);
+            return digits + CalculateChecksum(digits);
+        }
+
+        public static string GenerateNie() {
+            var index = new Random().Next(3);
+            var digits = GetRandomDigits(7);
+
+            return NieStartLetters[index] + digits + CalculateChecksum(index + digits);
+        }
+        #endregion
+
+        #region Validators
+
+        public static ValidationResult ValidateDni(string dni)
+        {
+            return ValidateNif(dni);
+        }
+
+        public static ValidationResult ValidateNif(string nif)
+        {
+            var nifRegex = new Regex("^[0-9]{8}[a-zA-Z]{1}$");
+	        if (!nifRegex.IsMatch(nif))
+            {
+                return ValidationResult.PatternMismatch;
+            }
+            
+            var letter = char.ToUpperInvariant(nif[8]);
+            var numbers = nif.Substring(0, 8);
+            var expected = CalculateChecksum(numbers);
+
+            if (letter != expected)
+            {
+                return ValidationResult.ChecksumMismatch;
+            } 
+
+            return ValidationResult.Valid;
+        }
+
+        public static ValidationResult ValidateNie(string nie) {
+	        var nieRegex = new Regex("^[xyzXYZ]{1}[0-9]{7}[a-zA-Z]{1}$");
+	        if (!nieRegex.IsMatch(nie)) {
+                return ValidationResult.PatternMismatch;
+            }
+
+            var startLetter = char.ToUpperInvariant(nie[0]);
+            var numbers = nie.Substring(1, 7);
+            var letter = char.ToUpperInvariant(nie[8]);
+
+	        var expected = CalculateChecksum(NieStartLetters.IndexOf(startLetter) + numbers);
+
+            if (letter != expected)
+            {
+                return ValidationResult.ChecksumMismatch;
+            }
+
+            return ValidationResult.Valid;
+        }
+
+        
+        #endregion
+        #region Helpers
+        private static char CalculateChecksum(string digits)
+        {
+            const string checkSumLetters = "TRWAGMYFPDXBNJZSQVHLCKE";
+            var index = (int)(long.Parse(digits) % 23);
+            return checkSumLetters[index];
+        }
+
+        private static string GetRandomDigits(int count)
+        {
+            var sb = new StringBuilder(count);
+            var rd = new Random();
+            for (var i = 0; i < count; i++)
+            {
+                sb.Append(rd.Next(10));
+            }
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+
+    public enum ValidationResult
+    {
+        PatternMismatch,
+        ChecksumMismatch,
+        Valid
+    }
+}
