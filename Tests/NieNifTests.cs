b001768954dc568673f2460f0a612fe9cee509cd
Immanuel Hayden <immanuel.hayden@gmail.com>
2015-03-11 13:41:45 +0100

    uppercase, bump version, nuget seems to be happier now

--- Tests/NieNifTests.cs
+++ Tests/NieNifTests.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using idgen.net;
+using Idgen.Net;
 
 namespace Tests
 {
--- idgen.net/IdGen.cs
+++ idgen.net/IdGen.cs
@@ -2,7 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace idgen.net
+namespace Idgen.Net
 {
     public static class IdGen
     {
